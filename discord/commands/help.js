6f9fec1a84b8a7c557c452c918273c154c701bdf
Luke Dunn <lukedunn011@gmail.com>
2024-11-26 01:19:52 +1100

    fixed anon error

--- discord/commands/help.js
+++ discord/commands/help.js
@@ -9,7 +9,8 @@ module.exports = {
         await interaction.reply({
             content: 'Available commands:\n' +
                      '`/help` - Shows this help message\n' +
-                     '`/be-yapper` - Gives you the yapper role\n',
+                     '`/be-yapper` - Gives you the yapper role\n' + 
+                     '`/anon` - Sends an anonymous message to the current channel',
             ephemeral: true
         });
     }
--- discord/commands/sendAnon.js
+++ discord/commands/sendAnon.js
@@ -4,20 +4,18 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('anon')
         .setDescription('Send an anonymous message to a channel')
-        .addStringOption(option => option.setName('message').setDescription('The message to send').setRequired(true))
-          
-}
+        .addStringOption(option => option.setName('message').setDescription('The message to send').setRequired(true)),
+    
+    async execute(interaction) {
+        const message = interaction.options.getString('message');
 
-async function execute(interaction) {
-    const message = interaction.options.getString('message');
-
-    try {
-        await interaction.reply({ content: 'Message sent anonymously.', ephemeral: true });
-
-        await interaction.channel.send({ content: `${message}` });
-    } catch (error) {
-        console.error('Error sending anonymous message:', error);
-        await interaction.reply({ content: 'An error occurred while sending the anonymous message.', ephemeral: true });
+        try {
+            await interaction.reply({ content: 'Message sent anonymously.', ephemeral: true });
+            await interaction.channel.send({ content: `Anonymous: ${message}` });
+        } catch (error) {
+            console.error('Error sending anonymous message:', error);
+            await interaction.reply({ content: 'An error occurred while sending the anonymous message.', ephemeral: true });
+        }
     }
-}
+};
 
